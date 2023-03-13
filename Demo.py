dfef21b131ba5829961ad117d13310c2c258df70
Rahuld27 <dixit.rahul94@gmail.com>
2023-03-13 23:18:26 +0530

    first commit

---
+++ Demo.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+st.title('Exar Fitness Trainer:  Squats Scanning')
+
+
+recorded_file = 'output_sample.mp4'
+sample_vid = st.empty()
+sample_vid.video(recorded_file)
+
+    
+    
+
+    
+
+
