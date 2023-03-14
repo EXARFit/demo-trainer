c44e3008c512ff52cb293a6b84d6882726932ad3
Rahuld27 <dixit.rahul94@gmail.com>
2023-03-14 14:29:56 +0530

    changes new

--- Demo.py
+++ Demo.py
@@ -1,16 +1,10 @@
 import streamlit as st
 
 
-st.title('Exar Fitness Trainer:  Squats Scanning')
-
-
-recorded_file = 'output_sample.mp4'
-sample_vid = st.empty()
-sample_vid.video(recorded_file)
+st.title('Welcome to Exar Fitness Trainer: Squats Scanning')
 
     
     
 
-    
 
 
